08aa2409baf077d91e27c4a61694f2d429651124
angelalita77 <anjeanette@gmail.com>
2025-10-06 03:39:28 -0400

    Adding state and actions for reducer.jsx

---
+++ src/reducer.jsx
@@ -0,0 +1,34 @@
+import { useReducer } from "react";
+
+export default function reducer(state, action) {
+
+    switch(action.type) {
+
+        /* add a new  item  */
+        case "ADD_TODO":{
+            return[
+                ...state,
+                {id: newID, item: action.payload, completed: false};
+            ];
+        }
+        
+        /*  delte an  item  */
+        case "DELETE_TODO": {
+            return[];
+        }
+        /* Check off completed item  */
+        case "CHECKOFF_TOTO": {
+            return[];
+        }
+        /* Edit item */
+        case "EDIT_TOTO": {
+            return[];
+        }
+
+        default:
+            return state;
+        }
+
+
+
+}
