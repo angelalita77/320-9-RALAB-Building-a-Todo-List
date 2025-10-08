d8f5c1560a802e7c2d52219f195f6aff110af889
angelalita77 <anjeanette@gmail.com>
2025-10-08 16:35:52 -0400

    Scrapping files and code due to bring over the code I was building outside of this repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 import { useState, useReducer } from "react"
 
-
 // Import components
-import TextInput from "./components/TextInput"
 import Header from "./components/Header"
-import todoReducer from "./todoReducer"
+
  
-// Import data (initialSet)
-import { initialState } from './data/initialState'
-import { prerenderToNodeStream } from "react-dom/static"
+
 
 
 function App() {
-  const[todos, dispatch] = useReducer(todoReducer, initialState)
-  console.log(prerenderToNodeStream)
-
-
+ 
 
   return (
     <div>
-      <Header />
-      <TextInput dispatch={todoReducer} />
-
-      {/* Putting the component structure of app */}
-      {/* <TodoList /> */}
+      <Todos />
     </div>
   )
 }
